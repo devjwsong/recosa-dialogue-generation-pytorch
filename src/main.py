9208790c1fc54b3b23fbaa9695b0b5d69e52b089
devJWSong <enflwodn@gmail.com>
2020-08-25 15:15:23 +0900

    Fresh restart.

--- src/main.py
+++ src/main.py
@@ -29,7 +29,8 @@ class Manager():
             'nucleus_p': 0.95,
             'ckpt_dir': 'saved_models',
             'pad_id': self.tokenizer._convert_token_to_id('Ġ'),
-            'inf': 1e6
+            'inf': 1e6,
+            'end_command': 'abort!'
         }
         
         # Load model & optimizer      
@@ -38,11 +39,14 @@ class Manager():
         self.optim = torch.optim.Adam(self.model.parameters(), lr=self.config['learning_rate'])
         self.best_loss = sys.float_info.max
         
+        if not os.path.exists(self.config['ckpt_dir']):
+            os.mkdir(self.config['ckpt_dir'])
+        
         if ckpt_name is not None:
             assert os.path.exists(f"{ckpt_dir}/{ckpt_name}"), f"There is no checkpoint named {ckpt_name}."
 
             print("Loading checkpoint...")
-            checkpoint = torch.load(f"{ckpt_dir}/{ckpt_name}")
+            checkpoint = torch.load(f"{self.config['ckpt_dir']}/{ckpt_name}")
             self.model.load_state_dict(checkpoint['model_state_dict'])
             self.optim.load_state_dict(checkpoint['optim_state_dict'])
             self.best_loss = checkpoint['loss']
@@ -50,7 +54,7 @@ class Manager():
             print("Initializing the model...")
             self.model.init_model()
               
-        if mode=='train':
+        if mode == 'train':
             # Load loss function
             print("Loading loss function...")
             self.criterion = nn.NLLLoss()
@@ -61,6 +65,10 @@ class Manager():
             valid_set = CustomDataset('valid', self.config['max_turn'], self.config['max_len'], self.config['pad_id'])
             self.train_loader = DataLoader(train_set, shuffle=True, batch_size=self.config['batch_size'])
             self.valid_loader = DataLoader(valid_set, shuffle=True, batch_size=self.config['batch_size'])
+        elif mode == 'test':
+            print("Loading the trained model...")
+            checkpoint = torch.load(f"{self.config['ckpt_dir']}/{ckpt_name}")
+            self.model.load_state_dict(checkpoint['model_state_dict'])
               
         print("Setting finished.")
               
@@ -69,7 +77,8 @@ class Manager():
               
         for epoch in range(1, self.config['epoch_nums']+1):
             self.model.train()
-              
+            
+            train_losses = []  
             for i, batch in enumerate(tqdm(self.train_loader)):
                 turn_num, dialogue = batch  # (B), (B, T, L)
                 turn_num, dialogue = turn_num.to(self.config['device']), dialogue.to(self.config['device'])
@@ -94,27 +103,26 @@ class Manager():
                         self.optim.step()
               
                         dialogue_losses.append(loss.item())
+                
+                train_losses += dialogue_losses
               
                 del turn_num, dialogue
                 torch.cuda.empty_cache()
               
-            mean_train_loss = np.mean(dialogue_losses)
+            mean_train_loss = np.mean(train_losses)
             print(f"#################### Epoch: {epoch} ####################")
             print(f"Train loss: {mean_train_loss}")
             
             valid_loss = self.validation()
               
             if valid_loss < self.best_loss:
-                if not os.path.exists(ckpt_dir):
-                    os.mkdir(ckpt_dir)
-              
                 state_dict = {
                     'model_state_dict': self.model.state_dict(),
                     'optim_state_dict': self.optim.state_dict(),
                     'loss': mean_train_loss
                 }
               
-                torch.save(state_dict, f"{ckpt_dir}/best_ckpt.tar")
+                torch.save(state_dict, f"{self.config['ckpt_dir']}/best_ckpt.tar")
                 print(f"***** Current best checkpoint is saved. *****")
                 self.best_loss = valid_loss
               
@@ -128,7 +136,6 @@ class Manager():
         self.model.eval()
               
         valid_losses = []
-        
         with torch.no_grad():
             for i, batch in enumerate(tqdm(self.valid_loader)):
                 turn_num, dialogue = batch  # (B), (B, T, L)
@@ -150,6 +157,8 @@ class Manager():
               
                         dialogue_losses.append(loss.item())
               
+                valid_losses += dialogue_losses
+                
                 del turn_num, dialogue
                 torch.cuda.empty_cache()
               
@@ -159,11 +168,43 @@ class Manager():
         
               
     def test(self):
-        pass
-              
-    def nucleus_sampling(self):
-        pass
+        print("Dialogue start!")
+        self.model.eval()
+        
+        context = None
+        print("Please type your input to start the conversation.")
+        print(f"If you want to finish this conversation, please type {self.config['end_command']}.")
+        while True:
+            input_sentence = input()
+            
+            if input_sentence == self.config['end_command']:
+                break
+            else:
+                tokens = self.tokenizer.tokenize(input_sentence)
+                ids = [self.tokenizer._convert_token_to_id(token) for token in tokens]
+                
+                if len(ids) <= self.config['max_len']:
+                    left = self.config['max_len'] - len(ids)
+                    ids += [self.config['pad_id']] * left
+                else:
+                    ids = ids[:self.config['max_len']]
+                    
+                x = torch.LongTensor(ids).unsqueeze(0)  # (1, L)
+                
+                if context is None:
+                    context = torch.zeros(dialogue.shape[0], self.config['hidden_size']).to(self.config['device'])
+                    
+                
+                
+                
+                
+        print("Dialogue finished. Thank you.")
 
+    def nucleus_sampling(self, x, context):
+        for pos in range(self.config['max_len']):
+            pass
+        
+        
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -179,3 +220,10 @@ if __name__=='__main__':
             manager = Manager(args.mode)
               
         manager.train()
+        
+    elif args.mode == 'test':
+        assert args.ckpt_name is not None, "Please specify the trained model checkpoint."
+        
+        manager = Manager(args.mode, ckpt_name=args.ckpt_name)
+        
+        manager.test()
