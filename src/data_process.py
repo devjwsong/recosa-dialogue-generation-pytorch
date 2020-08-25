9dc70061795f08b34e9f7d8e77e1359aea3c63f9
devJWSong <enflwodn@gmail.com>
2020-08-25 17:08:40 +0900

    Changed data preprocessing codes.

--- src/data_process.py
+++ src/data_process.py
@@ -1,27 +1,30 @@
 from tqdm import tqdm
 from torch.utils.data import Dataset
-from transformers import GPT2Tokenizer
 
 import torch
 import os
 import numpy as np
+import sentencepiece as spm
 
 
 # Parameters for data
 data_dir = 'data'
 raw_data_dir = 'raw'
-processed_data_dir = 'processed'
+sp_dir = 'trained_sp'
+sp_prefix = 'sp'
+processed_dir = 'processed'
 train_name = 'train'
 valid_name = 'validation'
 test_name = 'test'
 raw_name_prefix = 'dialogues'
 train_frac = 0.8
-pad = 'Ġ'
+pad = '<pad>'
+space = '\u2581'
 end_of_utterance = '__eou__'
-end_marks = ['.', ',', '?', '!']
+end_marks = ['.', ',', '?', '!', '...']
 quotes = ['"', '\'']
 abbreviations = ['s', 'd', 't', 'm', 're', 'll', 've', 'S', 'D', 'T', 'M', 'Re', 'Ll', 'Ve']
-dialogue_split_line = "#################################"
+dialogue_split_line = "[END OF DIALOGUE]"
 
 
 def merge_data(total_lines, data_path):
@@ -41,55 +44,69 @@ def resplit_data(total_lines):
 
 
 def process_token_list(token_list):
-    double_quote_count = 0
-    del_quote_idx = []
+    quote_count = 0
     for i, token in enumerate(token_list):
-        if pad in token:
-            if token[1:] in end_marks:
+        if space in token:
+            if token[1:] in end_marks or token[1:] in abbreviations:
                 token_list[i] = token[1:]
-
-                if i < len(token_list)-1:
-                    if token_list[i+1] not in end_marks and pad not in token_list[i+1]:
-                        token_list[i+1] = pad + token_list[i+1]
-            
-            if token == pad+quotes[1]:
-                if i < len(token_list)-1:
-                    if pad in token_list[i+1] and token_list[i+1][1:] in abbreviations:
-                        del_quote_idx.append(i)
-                        token_list[i+1] = '\''+token_list[i+1][1:]
-                    else:
-                        token_list[i] = token_list[i][1:]
-            
-            if (double_quote_count % 2 == 0 and token == quotes[0]+pad) \
-                    or (double_quote_count % 2 == 1 and token == pad+quotes[0]):
-                token_list[i] = quotes[0]
-                double_quote_count += 1
                 
-    if len(del_quote_idx) > 0:
-        new_token_list = [token_list[i] for i, token in enumerate(token_list) if i not in del_quote_idx]
-        token_list = new_token_list
+            if token[1:] == quotes[1]:
+                if i<len(token_list)-1:
+                    if token_list[i+1] in abbreviations or (token_list[i+1][0] == space and token_list[i+1][1:] in abbreviations):
+                        token_list[i] = token[1:]
+                        
+        if token[0] == space and token[1:] in quotes:
+            if quote_count % 2 == 1:
+                token_list[i] = token[1:]
+                quote_count = 0
+            else:
+                quote_count += 1
+                
+        if token in end_marks or token[1:] in end_marks:
+            if i<len(token_list)-1:
+                if token_list[i+1][0] != space:
+                    token_list[i+1] = space + token_list[i+1]
+                
+    new_token_list = [token for token in token_list if token != space]
         
-    return token_list
+    return new_token_list
 
 
 def save_data(lines, tokenizer, name):
-    with open(f"{data_dir}/{processed_data_dir}/{name}.txt", 'w') as f: 
-        for line in tqdm(lines):
-            dialogue = line.strip().replace(' __eou__ ', '__eou__')
-            dialogue = dialogue.replace(' __eou__', '__eou__')
-            dialogue = dialogue.replace('__eou__ ', '__eou__')
+    texts = []
+    ids = []
+    for line in tqdm(lines):
+        dialogue = line.strip().replace(' __eou__ ', '__eou__')
+        dialogue = dialogue.replace(' __eou__', '__eou__')
+        dialogue = dialogue.replace('__eou__ ', '__eou__')
 
-            utters = dialogue.split('__eou__')[:-1]
+        utters = dialogue.split('__eou__')[:-1]
+        dialogue_ids = []
+        
+        for utter in utters:
+            token_list = tokenizer.EncodeAsPieces(utter)
+            token_list = process_token_list(token_list)
             
-            for utter in utters:
-                token_list = tokenizer.tokenize(utter)
-                token_list = process_token_list(token_list)
-                
-                ids = [str(tokenizer._convert_token_to_id(token)) for token in token_list]
-                    
-                utter_idx = ' '.join(ids)
-                f.write(f"{utter_idx}\n")
-                
+            text = tokenizer.DecodePieces(token_list)
+            texts.append(text)
+            
+            token_ids = tokenizer.EncodeAsIds(text)
+            dialogue_ids.append(token_ids)
+        
+        texts.append(dialogue_split_line)
+        ids.append(dialogue_ids)
+    
+    print(f"Saving {name} text file...")
+    with open(f"{data_dir}/{processed_dir}/{name}.txt", 'w') as f:
+        for text in tqdm(texts):
+            f.write(f"{text}\n")
+    
+    print(f"Saving {name} id file...")
+    with open(f"{data_dir}/{processed_dir}/{name}_id.txt", 'w') as f:
+        for dialogue in tqdm(ids):
+            for utter in dialogue:
+                utter_str = [str(idx) for idx in utter]
+                f.write(f"{' '.join(utter_str)}\n")
             f.write(f"{dialogue_split_line}\n")
 
 
@@ -152,11 +169,12 @@ if __name__=='__main__':
     print("Respliting data...")
     train_lines, valid_lines = resplit_data(total_lines)
     
-    print("Loading GPT2 Tokenizer...")
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    print("Loading SentencePiece Tokenizer...")
+    tokenizer = spm.SentencePieceProcessor()
+    tokenizer.Load(f"{data_dir}/{sp_dir}/{sp_prefix}.model")
     
-    if not os.path.isdir(f"{data_dir}/{processed_data_dir}"):
-        os.mkdir(f"{data_dir}/{processed_data_dir}")
+    if not os.path.isdir(f"{data_dir}/{processed_dir}"):
+        os.mkdir(f"{data_dir}/{processed_dir}")
     
     print("Processing train utterances...")
     save_data(train_lines, tokenizer, train_name)
